c90ad47de0bc55811ba68bfac7fbe71899a9ccc0
Victor Anhaya <victoranhaya8@icloud.com>
2024-05-06 11:10:24 -0300

    Inicio do BackEnd

--- calculadora.php
+++ calculadora.php
@@ -1,3 +1,84 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['history'])) {
+    $_SESSION['history'] = [];
+}
+
+$result = "";
+if (isset($_GET['num1']) && isset($_GET['op'])) {
+    $num1 = (float)$_GET['num1']; 
+    $op = $_GET['op'];
+
+    if ($op !== '!') {
+        if (isset($_GET['num2'])) {
+            $num2 = (float)$_GET['num2'];
+            switch ($op) {
+                case '+':
+                    $result = $num1 + $num2;
+                    break;
+                case '-':
+                    $result = $num1 - $num2;
+                    break;
+                case '*':
+                    $result = $num1 * $num2;
+                    break;
+                case '/':
+                    if ($num2 == 0) {
+                        $result = "Erro: Divisão por zero";
+                    } else {
+                        $result = $num1 / $num2;
+                    }
+                    break;
+                case '^':
+                    $result = pow($num1, $num2);
+                    break;
+                default:
+                    $result = "Operação inválida";
+                    break;
+            }
+        } else {
+            $result = "Por favor, insira o segundo número.";
+        }
+    } else {
+        if ($num1 < 0) {
+            $result = "Fatorial de número negativo não é possível";
+        } else {
+            $result = 1;
+            for ($i = 2; i <= $num1; $i++) {
+                $result *= $i;
+            }
+        }
+    }
+
+    $calculation = "$num1 $op";
+    if ($op !== '!') {
+        $calculation .= " $num2 = $result";
+    } else {
+        $calculation .= " = $result";
+    }
+    $_SESSION['history'][] = $calculation;
+}
+
+if (isset($_GET['action'])) {
+    switch ($_GET['action']) {
+        case 'save':
+            if ($result !== "") {
+                $_SESSION['history'][] = "Salvo: $result";
+            }
+            break;
+        case 'get':
+            if (!empty($_SESSION['history'])) {
+                $result = end($_SESSION['history']); 
+            }
+            break;
+        case 'clear':
+            $_SESSION['history'] = [];
+            break;
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -8,7 +89,7 @@
 </head>
 <body>
     <div class="calc">
-        <form action="">
+        <form action="" method="GET">
             <label for="" class="texto-calc">Calculadora PHP</label>
             <div class="numeros">
                 <label class="num1" for="">Numero 1</label>
@@ -25,14 +106,21 @@
                 <input class="num" type="number" name="num2" id="num2">
             </div>
             <button class="enviar" type="submit">Calcular</button>
-            <div class="mostrar mostrar-calculo"></div>
-            <button class="enviar salvar" type="submit">Salvar</button>
-            <button class="enviar pegar" type="submit">Pegar valores</button>
+            <div class="mostrar mostrar-calculo">
+                <?php echo htmlspecialchars($result); ?>
+            </div>
+            <button class="enviar salvar" type="submit" name="action" value="save">Salvar</button>
+            <button class="enviar pegar" type="submit" name="action" value="get">Pegar valores</button>
             <button class="enviar m" type="submit">M</button>
-            <button class="enviar apagar" type="submit">Apagar histórico</button>
+            <button class="enviar apagar" type="submit" name="action" value="clear">Apagar histórico</button>
             <div class="texto-hist">Histórico</div>
-            <div class="mostrar mostrar-hist"></div>
-
+            <div class="mostrar mostrar-hist">
+                <?php
+                if (isset($result)) {
+                    echo htmlspecialchars($result); 
+                }
+                ?>
+            </div>
         </form>
     </div>
 </body>
